05ed84f42557869756aab0a05c43376e0d5b55fc
gustav-ops <leet1337@skiff.com>
2024-04-09 17:55:07 +0200

    Update __init__.py

--- pyxui/config_gen/__init__.py
+++ pyxui/config_gen/__init__.py
@@ -2,17 +2,35 @@ import json
 import base64
 from urllib.parse import urlencode
 
-def config_generator(protocol: str, config = dict, data: dict = None) -> str:
+
+def config_generator(protocol: str, config=dict, data: dict = None) -> str:
+
+    string = None
 
     match protocol:
         case "vmess":
-            string = "vmess://" + base64.b64encode(json.dumps(config).encode('utf-8')).decode('utf-8')
+            string = "vmess://" + base64.b64encode(
+                json.dumps(config).encode("utf-8")
+            ).decode("utf-8")
 
         case "vless":
-            string = "vless://{}@{}:{}?{}#{}".format(config['id'], config['add'], config['port'], urlencode(data), config['ps'])
+            string = "vless://{}@{}:{}?{}#{}".format(
+                config["id"],
+                config["add"],
+                config["port"],
+                urlencode(data),
+                config["ps"],
+            )
 
         case "shadowsocks":
-            string = "ss://{}:{}@{}:{}#{}".format(config['method'], config['server_password'], config['password'], config['server'], config['port'], config['ps'])
+            string = "ss://{}:{}@{}:{}#{}".format(
+                config["method"],
+                config["server_password"],
+                config["password"],
+                config["server"],
+                config["port"],
+                config["ps"],
+            )
 
         case _:
             string = None
