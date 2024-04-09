e6fc78020f39e10d14dc4b9602ff97f65b452d5f
gustav-ops <leet1337@skiff.com>
2024-04-09 17:32:52 +0200

    Update __init__.py
    add shadowsocks cfg generator

--- pyxui/config_gen/__init__.py
+++ pyxui/config_gen/__init__.py
@@ -2,11 +2,11 @@ import json
 import base64
 from urllib.parse import urlencode
 
-def config_generator(protocol: str, config = dict, data: dict = None) -> str:
+def config_generator(protocol: str, config: dict, data: dict = None) -> str:
     if protocol == "vmess":
         string = "vmess://" + base64.b64encode(json.dumps(config).encode('utf-8')).decode('utf-8')
-
     elif protocol == "vless":
         string = "vless://{}@{}:{}?{}#{}".format(config['id'], config['add'], config['port'], urlencode(data), config['ps'])
-
+    elif protocol == "shadowsocks":
+        string = "ss://{}:{}@{}:{}#{}".format(config['method'], config['server_password'], config['password'], config['server'], config['port'], config['ps'])
     return string
